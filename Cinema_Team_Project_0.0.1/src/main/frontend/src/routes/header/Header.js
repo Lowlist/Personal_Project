7458c6c5560606fda83cc02fcba6dba3f388db27
dud5825@naver.com <dud5825@naver.com>
2024-09-02 14:54:36 +0900

    [조영민] 2024-09-02 -헤더 html 작업중

---
+++ Cinema_Team_Project_0.0.1/src/main/frontend/src/routes/header/Header.js
@@ -0,0 +1,14 @@
+import style from './style/Header.module.css'
+
+function Header(){
+    return(
+        <div className={style.header}>
+            <div className={style.icon}>JO YOUNG MIN</div>
+            <div className={style.mainMenu}>
+                
+            </div>
+        </div>
+    )
+}
+
+export default Header;
