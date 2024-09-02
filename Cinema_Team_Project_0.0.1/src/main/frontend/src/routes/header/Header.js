b5690882796a81cd323d57eab880c29f652a0f24
dud5825@naver.com <dud5825@naver.com>
2024-09-02 16:10:33 +0900

    [조영민] 2024-09-02 -헤더 완성

--- Cinema_Team_Project_0.0.1/src/main/frontend/src/routes/header/Header.js
+++ Cinema_Team_Project_0.0.1/src/main/frontend/src/routes/header/Header.js
@@ -5,7 +5,10 @@ function Header(){
         <div className={style.header}>
             <div className={style.icon}>JO YOUNG MIN</div>
             <div className={style.mainMenu}>
-                
+                <div className={style.profile}>PROFILE</div>
+                <div className={style.portFolio}>PORTFOLIO</div>
+                <div className={style.github} onClick={()=>{}}>GITHUB</div>
+                <div className={style.moreBtn} onClick={()=>{}}>---</div>
             </div>
         </div>
     )
